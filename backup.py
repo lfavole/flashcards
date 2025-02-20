d9586d23acd5c307b75b932ab1a06c138d05156d
Laurent FAVOLE <laurentfavole03@gmail.com>
2025-02-20 13:38:35 +0100

    fix: date displayed in wrong time zone

--- backup.py
+++ backup.py
@@ -12,6 +12,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from pathlib import Path
 
+from pytz import timezone
+
 from progress import Progress
 from utils import CollectionWrapper
 
@@ -52,7 +54,7 @@ with tempfile.TemporaryDirectory() as export_dir:
     with Progress("Exporting all the collection"):
         output_file = wrapper.export(None, export_dir, full_backup=True)
 
-    now = dt.datetime.now()
+    now = dt.datetime.now().astimezone(timezone("Europe/Paris"))
     date = now.strftime("%d/%m/%Y %H:%M:%S")
     date_filename = now.strftime("%Y-%m-%d_%H-%M-%S")
     output_file = output_file.rename(output_file.parent / f"export_{date_filename}.apkg")
